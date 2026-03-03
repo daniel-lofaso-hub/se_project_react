aa18473716d4f9393a92d025ca70c5cb6497abf3
Daniel LoFaso <daniel.lofaso@gmail.com>
2026-03-03 13:09:57 -0500

    added weather and item cards

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,19 @@
+import { useState } from "react";
+
 import "./App.css";
 import Header from "../Header/Header";
+import Main from "../Main/Main";
+import Footer from "../Footer/Footer";
 
 function App() {
+  const [weatherData, setWeatherData] = useState({ type: "warm" });
+
   return (
     <div className="page">
       <div className="page__content">
         <Header />
+        <Main weatherData={weatherData} />
+        <Footer />
       </div>
     </div>
   );
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>Developed by Daniel LoFaso</p>
+      <p>2026</p>
+    </footer>
+  );
+}
+
+export default Footer;
