54c3b0793f9cef43e4c2efd1d8aff2a25ecdf0b7
Daniel LoFaso <daniel.lofaso@gmail.com>
2026-03-11 12:28:08 -0400

    finish weather cards

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -19,6 +19,46 @@ const weatherOptions = [
     condition: "clouds",
     url: new URL("../assets/night/cloudy.png", import.meta.url).href,
   },
+  {
+    day: true,
+    condition: "rain",
+    url: new URL("../assets/day/rain.png", import.meta.url).href,
+  },
+  {
+    day: false,
+    condition: "rain",
+    url: new URL("../assets/night/rain.png", import.meta.url).href,
+  },
+  {
+    day: true,
+    condition: "fog",
+    url: new URL("../assets/day/fog.png", import.meta.url).href,
+  },
+  {
+    day: false,
+    condition: "fog",
+    url: new URL("../assets/night/fog.png", import.meta.url).href,
+  },
+  {
+    day: true,
+    condition: "snow",
+    url: new URL("../assets/day/snow.png", import.meta.url).href,
+  },
+  {
+    day: false,
+    condition: "snow",
+    url: new URL("../assets/night/snow.png", import.meta.url).href,
+  },
+  {
+    day: true,
+    condition: "thunderstorm",
+    url: new URL("../assets/day/thunderstorm.png", import.meta.url).href,
+  },
+  {
+    day: false,
+    condition: "thunderstorm",
+    url: new URL("../assets/night/thunderstorm.png", import.meta.url).href,
+  },
 ];
 
 const defaultWeatherOptions = {
